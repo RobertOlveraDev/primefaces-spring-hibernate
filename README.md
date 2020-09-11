b20ffc38be0087e18ccfeb5769072e1709335e0d
Jose Roberto Olvera Perez <B02968@gfvepormas.com>
2020-09-10 22:59:08 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# primefaces-spring-hibernate
+ Template for Web Aplications based on Primefaces, Spring and Hibernate
---
+++ src/main/java/com/robolverap/hibernate/data/Employee.java
@@ -0,0 +1,61 @@
+package com.robolverap.hibernate.data;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column (name = "EMP_ID", nullable = false)
+	private long employeeId;
+	
+	@Column (name = "EMP_NAME")
+	private String employeeName;
+	
+	@Column (name = "EMP_HIRE_DATE")
+	private Date employeeHireDate;
+	
+	@Column (name = "EMP_SALARY")
+	private double employeeSalary;
+
+	public long getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(long employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public Date getEmployeeHireDate() {
+		return employeeHireDate;
+	}
+
+	public void setEmployeeHireDate(Date employeeHireDate) {
+		this.employeeHireDate = employeeHireDate;
+	}
+
+	public double getEmployeeSalary() {
+		return employeeSalary;
+	}
+
+	public void setEmployeeSalary(double employeeSalary) {
+		this.employeeSalary = employeeSalary;
+	}
+}
